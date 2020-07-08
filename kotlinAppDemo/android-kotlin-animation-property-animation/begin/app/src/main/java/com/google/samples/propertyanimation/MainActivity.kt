39e3575302681d69b73f1b1a80b71c870198d71e
tyy <yangyang.tian@5imakeup.com>
2020-07-08 15:21:51 +0800

    提交动画的demo

---
+++ kotlinAppDemo/android-kotlin-animation-property-animation/begin/app/src/main/java/com/google/samples/propertyanimation/MainActivity.kt
@@ -0,0 +1,172 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.samples.propertyanimation
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.animation.addListener
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var star: ImageView
+    lateinit var rotateButton: Button
+    lateinit var translateButton: Button
+    lateinit var scaleButton: Button
+    lateinit var fadeButton: Button
+    lateinit var colorizeButton: Button
+    lateinit var showerButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        star = findViewById(R.id.star)
+        rotateButton = findViewById<Button>(R.id.rotateButton)
+        translateButton = findViewById<Button>(R.id.translateButton)
+        scaleButton = findViewById<Button>(R.id.scaleButton)
+        fadeButton = findViewById<Button>(R.id.fadeButton)
+        colorizeButton = findViewById<Button>(R.id.colorizeButton)
+        showerButton = findViewById<Button>(R.id.showerButton)
+
+        rotateButton.setOnClickListener {
+            rotater()
+        }
+
+        translateButton.setOnClickListener {
+            translater()
+        }
+
+        scaleButton.setOnClickListener {
+            scaler()
+        }
+
+        fadeButton.setOnClickListener {
+            fader()
+        }
+
+        colorizeButton.setOnClickListener {
+            colorizer()
+        }
+
+        showerButton.setOnClickListener {
+            shower()
+        }
+    }
+
+    private fun rotater() {
+        //tyy  旋转
+        val animator = ObjectAnimator.ofFloat(star, View.ROTATION, -360f, 0f)
+        animator.duration = 1000
+//        disableViewDuringAnimation(rotateButton, animator)
+        animator.disableViewDuringAnimation(rotateButton)
+        animator.start()
+    }
+
+    private fun translater() {
+        //tyy 移动
+        val animator = ObjectAnimator.ofFloat(star, View.TRANSLATION_X, 200f)
+        animator.repeatCount = 3
+        animator.repeatMode = ObjectAnimator.REVERSE
+//        disableViewDuringAnimation(translateButton, animator)
+        animator.disableViewDuringAnimation(translateButton)
+        animator.start()
+    }
+
+    private fun scaler() {
+        //tyy 放缩
+        val scaleX = PropertyValuesHolder.ofFloat(View.SCALE_X, 4f)
+        val scaleY = PropertyValuesHolder.ofFloat(View.SCALE_Y, 4f)
+        val animator = ObjectAnimator.ofPropertyValuesHolder(
+            star, scaleX, scaleY
+        )
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(scaleButton)
+        animator.start()
+    }
+
+    private fun fader() {
+        val animator = ObjectAnimator.ofFloat(star, View.ALPHA, 0f)
+        animator.duration = 1000
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(fadeButton)
+        animator.start()
+    }
+
+    private fun colorizer() {
+        var animator = ObjectAnimator.ofInt(star.parent,
+            "backgroundColor", Color.BLACK, Color.RED)
+        animator.setDuration(500)
+        animator.repeatCount = 1
+        animator.repeatMode = ObjectAnimator.REVERSE
+        animator.disableViewDuringAnimation(colorizeButton)
+        animator.start()
+    }
+
+    private fun shower() {
+    }
+
+
+    /**
+     *
+     *实现监听方法一
+     */
+
+    private fun disableViewDuringAnimation(
+        view: View,
+        animator: ObjectAnimator
+    ) {
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                view.isEnabled = false
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                view.isEnabled = true
+            }
+        })
+    }
+
+    /**
+     *
+     *实现监听方法二
+     * 功能和方法一相同
+     */
+
+    private fun ObjectAnimator.disableViewDuringAnimation(view: View) {
+        addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationStart(animation: Animator?) {
+                view.isClickable = false
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                view.isClickable = true
+            }
+
+        })
+    }
+}
